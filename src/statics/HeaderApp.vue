8ac1bef43f54939e160e363d3d73a181799003a0
Julien Noyer <j.noyer@digitalworkshop.fr>
2021-03-17 12:07:58 +0100

    Modification du header

--- src/statics/HeaderApp.vue
+++ src/statics/HeaderApp.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="flexBox flexColumn">
-      <h1>Node.js Boiler plate by <a href="https://dwsapp.io" target="_blank">dwsapp.io</a></h1>
+      <h1>VueJS Boiler plate by <a href="https://dwsapp.io" target="_blank">dwsapp.io</a></h1>
   </header>
 </template>
 
